5a458599437a03d8f1129cbb78a1f295f141df36
Cyril Diagne <cyril.diagne@ecal.ch>
2018-03-11 00:24:52 +0100

    add hosted link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Online demo/test bed for Prepro pipeline.
+
+Alpha: [prepro-demo.firebaseapp.com](https://prepro-demo.firebaseapp.com)
--- static/js/login.js
+++ static/js/login.js
@@ -1,5 +1,7 @@
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
 const uiConfig = {
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  accountChooserEnabled: false,
   signInSuccessUrl: 'index.html',
   signInOptions: [{
     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,

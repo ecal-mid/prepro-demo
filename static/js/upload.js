b17ba0deeccce991e4f2290a17a42be33dedbc0f
Cyril Diagne <cyril.diagne@ecal.ch>
2018-06-01 12:13:08 +0200

    allow files up to 100mo

--- static/js/upload.js
+++ static/js/upload.js
@@ -19,7 +19,7 @@ function fileChanged(evt) {
   if (!file) {
     return;
   }
-  if (file.size / (1000 * 1000) > 10) {
+  if (file.size / (1000 * 1000) > 100) {
     alert('The maximum file size is currently limited to 10mo.');
     evt.target.value = null;
     return;
